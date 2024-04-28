0b776bdc85358da708a7a3986bb56a509884512c
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-04-28 14:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+});
